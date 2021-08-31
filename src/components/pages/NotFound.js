2d6a8acac5d6b4772d3af5daa85cd88738efe00e
Jaydeep <jaydeepshelake10@gmail.com>
2021-08-31 17:52:26 +0530

    changed images

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -19,8 +19,8 @@ useEffect(()=>{
         <>
         <Header/>
         <div className="confirmed-order">
-            <img src={img} alt="" />
-            <h1 className="text-2xl font-bold mt-16 mb-6">Order has been placed!</h1>
+            <img src={img} alt="" style={{width:'213px',height:'82px'}} />
+            <h1 className="text-2xl font-bold mt-16 mb-6">Page Not Found</h1>
             <p>You will be redirected in {counter}s..</p>
         </div>
         </>
